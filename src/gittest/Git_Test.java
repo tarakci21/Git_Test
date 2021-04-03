e482f5788416af6a9fc724fd37a7584c90f866f0
tarakci21 <efetararci@gmail.com>
2021-04-03 19:53:42 +0300

    version3

--- src/gittest/Git_Test.java
+++ src/gittest/Git_Test.java
@@ -5,6 +5,8 @@ public class Git_Test {
 	public static void main(String[] args) {
 		System.out.println("Git den merhaba");
 		System.out.println("Git Hub dan merhaba");
+		
+		System.out.println("Git Dersi Baþladý");
 
 	}
 
